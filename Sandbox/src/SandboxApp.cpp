7f43aa1a52fafe56e3eb8ff03c76920635ff15e7
SYNTAXDZ <miloudiadelhani@gmail.com>
2020-08-23 12:50:30 +0100

    Added shader assets and more..

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -3,31 +3,88 @@
 
 class ExampleLayer : public FGE::Layer {
 
-    public:
-        ExampleLayer() : Layer( "ExampleLayer" ) {}
+public:
+  ExampleLayer() : Layer( "ExampleLayer" ), m_CameraController( 800.0f / 600.0f ) {}
 
-        virtual void OnUpdate() override {
+  virtual void OnAttach() override {
 
-            if( FGE::Input::IsKeyPressed( FGE_KEY_SPACE ) )
-                FGE_INFO( "Key Pressed" );
+      m_VertexArray = FGE::VertexArray::Create();
 
-            //FGE_INFO( "ExampleLayer::OnUpdate" );
+      m_Shader = FGE::Shader::Create( "/home/syntax/Base/Knowledge/learn_cpp/CppProjects/FirstGameEngine/Sandbox/assets/shaders/FlatColor.glsl" );
 
-        }
+      float vertices[] = {
 
-        virtual void OnImGuiRender() override {
+        -0.5f, -0.5f, 0.0f, 0.5f, -0.5f,
+        0.0f,  0.0f,  0.5f, 0.0f
 
-            ImGui::Begin( "Window" );
-            ImGui::Text( "This is a Text" );
-            ImGui::End();
+    };
+    uint32_t indices[] = {0, 1, 2};
 
-        }
+    m_VertexBuffer = FGE::VertexBuffer::Create( vertices, sizeof( vertices ) );
 
-        virtual void OnEvent( FGE::Event& event ) override {
+    FGE::BufferLayout layout = {
 
-            //FGE_TRACE( "{0}", event );
+        {FGE::ShaderDataType::Float3, "a_Position"}
 
-        }
+    };
+    m_VertexBuffer->SetLayout( layout );
+
+    // 12/ 4 = 3 (count of indicies)
+    m_IndexBuffer = FGE::IndexBuffer::Create( indices, sizeof( indices ) / sizeof( uint32_t ) );
+
+    m_VertexArray->AddVertexBuffer( m_VertexBuffer );
+    m_VertexArray->SetIndexBuffer( m_IndexBuffer );
+
+    m_Shader->Bind();
+
+    glm::vec4 color = glm::vec4( 0.1f, 0.9f, 0.3f, 1.0f );
+    m_Shader->SetFloat4("u_Color", color );
+
+  }
+
+    virtual void OnUpdate( FGE::Timestep ts ) override {
+
+        m_CameraController.OnUpdate( ts );
+
+        FGE::RenderCommand::Clear();
+        FGE::RenderCommand::SetClearColor( glm::vec4( 0.2f, 0.3f, 0.3f, 1.0f ) );
+
+        glm::mat4 projection = m_CameraController.GetCamera().GetProjectionMatrix();
+        glm::mat4 view = m_CameraController.GetCamera().GetViewMatrix();
+        glm::mat4 model = glm::mat4( 1.0f );
+
+        m_Shader->SetMat4( "u_Projection", projection );
+        m_Shader->SetMat4( "u_View", view );
+        m_Shader->SetMat4( "u_Model", model );
+
+        FGE::Renderer::BeginScene();
+
+        FGE::Renderer::Submit( m_VertexArray, m_Shader );
+
+        FGE::Renderer::EndScene();
+
+    }
+
+    virtual void OnImGuiRender() override {
+
+        ImGui::Begin( "Window" );
+        ImGui::Text( "This is a Text" );
+        ImGui::End();
+
+    }
+
+    virtual void OnEvent( FGE::Event& event ) override {
+
+        //FGE_TRACE( "{0}", event );
+
+    }
+
+private:
+    FGE::Ref<FGE::VertexBuffer> m_VertexBuffer;
+    FGE::Ref<FGE::IndexBuffer> m_IndexBuffer;
+    FGE::Ref<FGE::VertexArray> m_VertexArray;
+    FGE::CameraController m_CameraController;
+    FGE::Ref<FGE::Shader> m_Shader;
 
 };
 
