d935ff6844f3ed3e2af149204f0f6d5b1609fd8c
Your Name <you@example.com>
2020-11-14 14:28:29 +0100

    added dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,14 +1,14 @@
-((nil . ((cmake-ide-project-dir . "~/Base/Knowledge/learn_cpp/CppProjects/FirstGameEngine")
-        (cmake-ide-build-dir . "~/Base/Knowledge/learn_cpp/CppProjects/FirstGameEngine/build-emacs")
-        (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE='~/Programs/vcpkg/scripts/buildsystems/vcpkg.cmake' -DPORTABLE=1 -DCMAKE_CXX_COMPILER='/usr/bin/clang++'")
+((nil . ((cmake-ide-project-dir . "~/Base/Knowledge/learn_cpp/FirstGameEngine")
+        (cmake-ide-build-dir . "~/Base/Knowledge/learn_cpp/FirstGameEngine/build")
+        (cmake-ide-cmake-opts . "-DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=ON -DCMAKE_TOOLCHAIN_FILE='~/Programs/vcpkg/scripts/buildsystems/vcpkg.cmake' -DCMAKE_CXX_COMPILER='/usr/bin/clang++'")
         (projectile-project-name . "FirstGameEngine")
         (projectile-project-run-cmd . "./run.sh")
         (projectile-project-test-cmd . "./test.sh")
-        (setq helm-ctest-build-dir "~/Base/Knowledge/learn_cpp/CppProjects/FirstGameEngine/build-emacs")
+        (setq helm-ctest-build-dir "~/Base/Knowledge/learn_cpp/FirstGameEngine/build")
         (helm-make-build-dir . "build")
         (helm-make-arguments . "-j4"))))
 
-;; ((c++-mode . ((lsp-clients-clangd-args . ("--compile-commands-dir=build-emacs"
+;; ((c++-mode . ((lsp-clients-clangd-args . ("--compile-commands-dir=build"
                                           ;; "--pch-storage=memory"
                                           ;; "--background-index"
                                           ;; "-j=4")))))
